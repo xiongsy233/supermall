b1d7b615cff9d1b3f38b5cafdbdb2eac8d600b46
xiongsy <xiongsy@163.com>
2021-08-28 14:23:42 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    configureWebpack:{
-        resolve:{
-            // 给文件名配置别名 @默认为src文件
-            alias:{
-                'asseets': '@/assets',
-                'common': '@/common',
-                'components': '@/components',
-                'network': '@/network',
-                'views': '@/views'
-            }
+    configureWebpack: {
+      resolve: {
+        alias: {
+          'assets': '@/assets',
+          'common': '@/common',
+          'components': '@/components',
+          'network': '@/network',
+          'views': '@/views',
         }
+      }
     }
-}
+  }
+  
+  
