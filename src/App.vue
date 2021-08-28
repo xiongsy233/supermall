cbfed9b1163693839590fffaa998a02c972d577d
xiongsy <xiongsy@163.com>
2021-08-28 14:02:57 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div id="app">
+    <main-tab-bar></main-tab-bar>
+  </div>
 </template>
 
 <script>
-
+import MainTabBar from 'components/content/mainTabbar/MainTabBar.vue'
 export default {
   name: 'App',
   components: {
-   
+    MainTabBar
   }
 }
 </script>
