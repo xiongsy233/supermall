19ff358daec1e1136c2f7cee3436bc5807d7e14d
xiongsy <xiongsy@163.com>
2021-08-28 10:47:22 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configurewebpack:{
+        resolve:{
+            // 给文件名配置别名 @默认为src文件
+            alias:{
+                'asseets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
