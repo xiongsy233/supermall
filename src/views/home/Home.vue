56becbeb5da0683e16c7486be53d763eae4cd5fc
xiongsy <xiongsy@163.com>
2021-08-28 16:09:17 +0800

    123

---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>首页</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+        }
+    },
+    watch:{
+    },
+    created() {
+    },
+    methods:{
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
